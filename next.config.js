25466c094e829c2441d5e46a18bffd41b38433ed
Victor Canto <iamvictorcanto@gmail.com>
2022-10-16 15:45:01 -0300

    chore: installed unsplash-js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   swcMinify: true,
 
   images: {
-    domains: ['images.unsplash.com']
-  }
-}
+    domains: ['images.unsplash.com', 'api.foursquare.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
